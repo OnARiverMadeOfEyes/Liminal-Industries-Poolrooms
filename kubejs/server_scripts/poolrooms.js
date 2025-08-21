f14c4808529c4f23edb1c550e7735fc6cb9b5e77
markaabo <68380434+markaabo@users.noreply.github.com>
2025-08-21 20:15:47 +0200

    Update poolrooms.js to have both types of locker

--- kubejs/server_scripts/poolrooms.js
+++ kubejs/server_scripts/poolrooms.js
@@ -1,23 +1,45 @@
 //Pool Locker
 
-BlockEvents.placed('kubejs:pool_locker', event => {
+BlockEvents.placed('kubejs:pool_locker_single', event => {
     const {block, entity} = event
     const topHalf = block['up']
     if(topHalf.id != 'minecraft:air'){
         event.cancel()
         return
     }else{
-        topHalf.set('kubejs:pool_locker', {
+        topHalf.set('kubejs:pool_locker_single', {
             facing: block.properties.facing,
             half: 'top'
         })
     }
 })
 
-BlockEvents.broken('kubejs:pool_locker', event => {
+BlockEvents.broken('kubejs:pool_locker_single', event => {
     const {block, entity} = event
     const otherHalf = block.properties.half == 'bottom' ? block['up'] : block['down']
-    if(otherHalf.id == 'kubejs:pool_locker'){
+    if(otherHalf.id == 'kubejs:pool_locker_single'){
+        otherHalf.set('minecraft:air')
+    }
+})
+
+BlockEvents.placed('kubejs:pool_locker_double', event => {
+    const {block, entity} = event
+    const topHalf = block['up']
+    if(topHalf.id != 'minecraft:air'){
+        event.cancel()
+        return
+    }else{
+        topHalf.set('kubejs:pool_locker_double', {
+            facing: block.properties.facing,
+            half: 'top'
+        })
+    }
+})
+
+BlockEvents.broken('kubejs:pool_locker_double', event => {
+    const {block, entity} = event
+    const otherHalf = block.properties.half == 'bottom' ? block['up'] : block['down']
+    if(otherHalf.id == 'kubejs:pool_locker_double'){
         otherHalf.set('minecraft:air')
     }
 })
